f33a5f1f0cfa26046cc513e2e40a63385ebd1ee2
Tom Laflin <Tom@DESKTOP-9DD565D>
2018-01-27 13:01:50 -0800

    Added prefabs for player and enemy.  When player spike collides with enemy, enemy is destroyed.

---
+++ Global Game Jam/Assets/SpikeCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollider : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		bool isPlayer = gameObject.CompareTag("Player");
+		bool otherIsPlayer = other.gameObject.CompareTag("Player");
+
+		if (isPlayer != otherIsPlayer)
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
