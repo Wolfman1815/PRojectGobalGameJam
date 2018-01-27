9959c72cb9a4e2920eb0b47c7e0332ae287bde61
Tom Laflin <Tom@DESKTOP-9DD565D>
2018-01-27 13:14:35 -0800

    Updates to hopefully fix my broken git state???

---
+++ Global Game Jam/Assets/Evan/EnemyChase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour {
+
+    public Transform[] PatrolPoints;
+    public float speed;
+    Transform currentPatrolPoints;
+    int currentPatrolIndex;
+
+    public Transform target;
+    public float chaseRange;
+   
+    // Use this for initialization
+    void Start () {
+        currentPatrolIndex = 0;
+        currentPatrolPoints = PatrolPoints[currentPatrolIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if(distanceToTarget < chaseRange){
+            Vector3 targetDir = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+	}
+}
