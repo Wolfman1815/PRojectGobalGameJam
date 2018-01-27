8fd23918933141a9c8e18be80bd446f0f499f6ca
Tom Laflin <Tom@DESKTOP-9DD565D>
2018-01-27 15:29:14 -0800

    Fixed the spikes to stay attached to the character while it moves and rotates.

--- Global Game Jam/Assets/Scripts/AttackController.cs
+++ Global Game Jam/Assets/Scripts/AttackController.cs
@@ -22,31 +22,31 @@ public class AttackController : MonoBehaviour
 	private IEnumerator Attack()
 	{
 		var initialPosition = AttackObject.transform.position;
-		var targetPosition = initialPosition + AttackObject.transform.rotation * new Vector3(0, 1);
-		yield return MoveOverTime(targetPosition, 0.06f);
+		var targetRelativePosition = new Vector3(0, 1);
+		yield return MoveOverTime(targetRelativePosition, 0.06f);
 
 		yield return new WaitForSeconds(0.3f);
 
-		yield return MoveOverTime(initialPosition, 0.06f);
+		yield return MoveOverTime(Vector3.zero, 0.06f);
 		m_active = false;
 		AttackObject.SetActive(false);
 	}
 
-	private IEnumerator MoveOverTime(Vector3 targetPosition, float durationSeconds)
+	private IEnumerator MoveOverTime(Vector3 targetRelativePosition, float durationSeconds)
 	{
-		var initialPosition = AttackObject.transform.position;
 		float elapsedTime = 0;
 
 		while (elapsedTime < durationSeconds)
 		{
+			var currentRotation = AttackObject.transform.localRotation;
 			elapsedTime += Time.deltaTime;
 			if (elapsedTime >= durationSeconds)
 			{
-				AttackObject.transform.position = targetPosition;
+				AttackObject.transform.localPosition = currentRotation * targetRelativePosition;
 			}
 			else
 			{
-				AttackObject.transform.position = initialPosition + (targetPosition - initialPosition) * elapsedTime / durationSeconds;
+				AttackObject.transform.localPosition = currentRotation * targetRelativePosition * elapsedTime / durationSeconds;
 			}
 
 			yield return null;
