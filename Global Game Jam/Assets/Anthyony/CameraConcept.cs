16749090d40c71f600917faf1f72284e0bb3fcae
anthony <anthony.ngo@bellevuecollege.edu>
2018-01-27 12:47:31 -0800

    player movement. -AN

---
+++ Global Game Jam/Assets/Anthyony/CameraConcept.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConcept : MonoBehaviour {
+	public float Speed = 1;
+	private Vector3 pos;
+	private float x;
+	private float y;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		x = Input.GetAxis ("Horizontal") * Speed;
+		y = Input.GetAxis ("Vertical") * Speed;
+		pos = transform.position;
+		pos.x += x;
+		pos.y += y;
+		transform.position = pos;
+
+		//follow mouse
+		Vector2 playerPos = Camera.main.WorldToViewportPoint (transform.position);
+		Vector2 mousePos = (Vector2)Camera.main.ScreenToViewportPoint (Input.mousePosition);
+		float angle = angleBetween (playerPos, mousePos);
+		transform.rotation = Quaternion.Euler (0, 0, angle);
+	}
+
+	float angleBetween (Vector3 a, Vector3 b) {
+		return Mathf.Atan2 (a.y-b.y, a.x-b.x)*Mathf.Rad2Deg;
+	}
+}
