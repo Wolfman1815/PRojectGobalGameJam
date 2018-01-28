c623c4dcdf3e2fc4cbc5e7fe2be8cac7dab506b3
Tom Laflin <Tom@DESKTOP-9DD565D>
2018-01-28 12:17:07 -0800

    Added NextLevel object with script to load the next level once there are no objects tagged as enemies.

---
+++ Global Game Jam/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+	void Update()
+	{
+		var livingEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		if (livingEnemies.Length == 0)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
